27ffad1dcb8c2f6dda4d1b4ae896b8b606b9496e
ixayman <evoix.a@gmail.com>
2024-06-26 11:09:49 +0300

    v3

--- exc-1/test_math_function.py
+++ exc-1/test_math_function.py
@@ -4,57 +4,51 @@ import math_function
 
 
 class TestModuloFunc(unittest.TestCase):
-    def test_even_result(self):
+    def test_modulo_func_even_result(self):
         num = 2
         result = math_function.modulo_func(num)
         self.assertEqual(0, result, "wrong result - should be even")
 
-    def test_odd_result(self):
+    def test_modulo_func_odd_result(self):
         num = 7
         result = math_function.modulo_func(num)
         self.assertNotEqual(0, result, "wrong result - should be odd")
 
-
-class TestIsEven(unittest.TestCase):
-    def test_true_result(self):
+    def test_is_even_true_result(self):
         num = 2
         result = math_function.is_even(num)
         self.assertTrue(result, "wrong result - should be true")
 
-    def test_false_result(self):
+    def test_is_even_false_result(self):
         num = 1
         result = math_function.is_even(num)
         self.assertFalse(result, "wrong result - should be false")
 
-
-class TestIsOdd(unittest.TestCase):
-    def test_true_result(self):
+    def test_is_odd_true_result(self):
         num = 1
         result = math_function.is_odd(num)
         self.assertTrue(result, "wrong result - should be true")
 
-    def test_false_result(self):
+    def test_is_odd_false_result(self):
         num = 2
         result = math_function.is_odd(num)
         self.assertFalse(result, "wrong result - should be false")
 
-
-class TestSumFunction(unittest.TestCase):
-    def test_sum_positive_numbers(self):
+    def test_sum_function_positive_numbers(self):
         num_a = 1
         num_b = 2
         summ = 3
         result = math_function.sum_function(num_a, num_b)
         self.assertEqual(summ, result, "positive numbers - wrong sum")
 
-    def test_sum_negative_numbers(self):
+    def test_sum_function_negative_numbers(self):
         num_a = -1
         num_b = -2
         summ = -3
         result = math_function.sum_function(num_a, num_b)
         self.assertEqual(summ, result, "negative numbers - wrong sum")
 
-    def test_sum_positive_and_negative(self):
+    def test_sum_function_positive_and_negative(self):
         num_a = [1, -1]
         num_b = [-2, 2]
         summ = [-1, 1]
@@ -62,7 +56,7 @@ class TestSumFunction(unittest.TestCase):
             result = math_function.sum_function(num_a[i], num_b[i])
             self.assertEqual(summ[i], result, "negative & positive numbers - wrong sum")
 
-    def test_sum_with_zero(self):
+    def test_sum_function_with_zero(self):
         num_a = [0, 0, 1, -1, 0]
         num_b = [0, 1, 0, 0, -1]
         summ = [0, 1, 1, -1, -1]
@@ -70,30 +64,28 @@ class TestSumFunction(unittest.TestCase):
             result = math_function.sum_function(num_a[i], num_b[i])
             self.assertEqual(summ[i], result, "sum with zero - wrong sum")
 
-    def test_sum_large_numbers(self):
+    def test_sum_function_large_numbers(self):
         num_a = 1000000
         num_b = 2000000
         summ = 3000000
         result = math_function.sum_function(num_a, num_b)
         self.assertEqual(summ, result, "sub large numbers - wrong sum")
 
-
-class TestSubFunction(unittest.TestCase):
-    def test_sum_positive_numbers(self):
+    def test_sub_function_positive_numbers(self):
         num_a = 2
         num_b = 1
         subb = 1
         result = math_function.sub_function(num_a, num_b)
         self.assertEqual(subb, result, "positive numbers - wrong sub result")
 
-    def test_sum_negative_numbers(self):
+    def test_sub_function_negative_numbers(self):
         num_a = -1
         num_b = -2
         subb = 1
         result = math_function.sub_function(num_a, num_b)
         self.assertEqual(subb, result, "negative numbers - wrong sub result")
 
-    def test_sum_positive_and_negative(self):
+    def test_sub_function_positive_and_negative(self):
         num_a = [1, -1]
         num_b = [-2, 2]
         subb = [3, -3]
@@ -101,7 +93,7 @@ class TestSubFunction(unittest.TestCase):
             result = math_function.sub_function(num_a[i], num_b[i])
             self.assertEqual(subb[i], result, "negative & positive numbers - wrong sub result")
 
-    def test_sum_with_zero(self):
+    def test_sub_function_with_zero(self):
         num_a = [0, 0, 1, -1, 0]
         num_b = [0, 1, 0, 0, -1]
         subb = [0, -1, 1, -1, 1]
@@ -109,7 +101,7 @@ class TestSubFunction(unittest.TestCase):
             result = math_function.sub_function(num_a[i], num_b[i])
             self.assertEqual(subb[i], result, "sub with zero - wrong sub result")
 
-    def test_sum_large_numbers(self):
+    def test_sub_function_large_numbers(self):
         num_a = 2000000
         num_b = 1000000
         subb = 1000000
