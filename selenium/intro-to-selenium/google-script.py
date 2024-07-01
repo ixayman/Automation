9c1d1493d8303b5ad17855a2684cd7bd44132291
ixayman <evoix.a@gmail.com>
2024-07-01 14:52:07 +0300

    sel

---
+++ selenium/intro-to-selenium/google-script.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+driver = webdriver.Firefox()
+
+
+driver.get("https://www.google.com")
+
+
+search_input = driver.find_element(By.NAME, 'q')
+
+
+search_input.send_keys("Python programming")
+search_input.send_keys(Keys.ENTER)
+time.sleep(2)
+first_result = driver.find_element(By.CSS_SELECTOR, "h3")
+first_result.click()
+driver.quit()
---
+++ selenium/intro-to-selenium/youtube-script.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+driver = webdriver.Firefox()
+
+
+
+driver.get("https://www.youtube.com")
+
+
+search_input = driver.find_element(By.XPATH, '//input[@id="search"]')
+
+search_input.send_keys("hello")
+search_input.send_keys(Keys.ENTER)
+
+
+driver.quit()
