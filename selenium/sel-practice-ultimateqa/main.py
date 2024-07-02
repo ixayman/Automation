fbc3daa12015af10014b553ac60641563a0a326f
ixayman <evoix.a@gmail.com>
2024-07-02 11:16:46 +0300

    sel

---
+++ selenium/sel-practice-ultimateqa/main.py
@@ -0,0 +1,8 @@
+import funcs
+
+if __name__ == '__main__':
+    funcs.click_me_button()
+    funcs.go_to_login_page_button()
+    funcs.click_using_id_button()
+    funcs.radio_buttons()
+    funcs.email_me()
