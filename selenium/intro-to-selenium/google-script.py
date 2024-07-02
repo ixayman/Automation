6406e703d0dc4a07cc4b8c33d14eee5475ff1ebe
ixayman <evoix.a@gmail.com>
2024-07-02 10:10:23 +0300

    sel

--- selenium/intro-to-selenium/google-script.py
+++ selenium/intro-to-selenium/google-script.py
@@ -7,13 +7,13 @@ driver = webdriver.Firefox()
 
 driver.get("https://www.google.com")
 
-
+time.sleep(3)
 search_input = driver.find_element(By.NAME, 'q')
 
 
 search_input.send_keys("Python programming")
 search_input.send_keys(Keys.ENTER)
-time.sleep(2)
+time.sleep(3)
 first_result = driver.find_element(By.CSS_SELECTOR, "h3")
 first_result.click()
 driver.quit()
