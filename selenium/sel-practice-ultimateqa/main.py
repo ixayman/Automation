50e808c15506e72863fadd2466d91fba9464127c
ixayman <evoix.a@gmail.com>
2024-07-02 12:27:17 +0300

    sel

--- selenium/sel-practice-ultimateqa/main.py
+++ selenium/sel-practice-ultimateqa/main.py
@@ -1,4 +1,5 @@
 import funcs
+import funcs2
 
 if __name__ == '__main__':
     funcs.click_me_button()
@@ -6,3 +7,6 @@ if __name__ == '__main__':
     funcs.click_using_id_button()
     funcs.radio_buttons()
     funcs.email_me()
+    funcs2.button_count()
+    funcs2.social_media_follows()
+    #funcs2.fill_name_email_message_captcha(`)
